c0be25081aba16011b91f758c31b7fa9c8ecd72e
Khushnood Ali Khan <khushnoodali287@gmail.com>
2026-08-18 20:51:54 +0500

    Intro

---
+++ Lesson/1_intro.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# CHECK VERSION
+# print(np.__version__)
+
+# CREATE NUMPY ARRAY
+arry = np.array([1,3,4,2,4])
+print(arry)
+print(type(arry))
+
+# YOU CAN"T MULTIPLY PYTHON LIST BY A NUMBER BUT ARRAY CAN BE MULTIPIED
+arry = arry * 2
+print(arry)
