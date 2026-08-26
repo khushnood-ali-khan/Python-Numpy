3196b4da78fa93b7fce4434365990980cd226513
Khushnood Ali Khan <khushnoodali287@gmail.com>
2026-08-27 00:07:12 +0500

    Random

--- Lesson/8_Random.py
+++ Lesson/8_Random.py
@@ -2,21 +2,20 @@ import numpy as np
 
 # # Genrate Random value with numpy built-in funtions
 
-# rng = np.random.default_rng()
+rng = np.random.default_rng()
 
-# print(rng.integers(1, 11))      # random number btw 1 to 10
-# print(rng.integers(1, 11, 3))      # random number btw 1 to 10 the size of 3
+print(rng.integers(1, 11))      # random number btw 1 to 10
+print(rng.integers(1, 11, 3))      # random number btw 1 to 10 the size of 3
 
 # # For readibility you can assign key words like:
-# print(rng.integers(low=10, high= 101, size= 5))
+print(rng.integers(low=10, high= 101, size= 5))
 
 # # if you want 2 dimentional random numbers, then you can set dimentions in size
-# print(rng.integers(low=1, high=51, size=(3,2)))
-
+print(rng.integers(low=1, high=51, size=(3,2)))
 
 # # IF YOU WANT TO GENRATE THE SAME RANDOM LIST EVERY TIME YOU CAN USE seed IN RANDOM FUNCTION
-# rng2 = np.random.default_rng(seed=1)
-# print(rng2.integers(low=1, high=21, size=(2,3)))    # it will always genrate the same random array
+rng2 = np.random.default_rng(seed=1)
+print(rng2.integers(low=1, high=21, size=(2,3)))    # it will always genrate the same random array
 
 
 # GENERATE RANDOM floating NUMBERS
@@ -27,10 +26,14 @@ np.random.seed(1)
 print(np.random.uniform(low=-1, high=1, size=(3,2)))
 
 # Shuffle our array
-
 rg = np.random.default_rng()
-
 arr = np.array([1,2,4,5,6,7])
-
 rg.shuffle(arr)
 print(arr)
+
+# Choose Random choice from array
+
+fruit_rng = np.random.default_rng()
+fruits = np.array(['Apple', 'Mango', 'Barry', ' Banana'])
+choose = fruit_rng.choice(fruits)       # you can even define size
+print(choose)
